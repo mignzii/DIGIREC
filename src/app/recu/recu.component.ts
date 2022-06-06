ebd9a300dd2301157a4cd89f633614d06a906a35
mignzii <48212077+mignzii@users.noreply.github.com>
2022-06-06 17:21:50 +0100

    lest update

--- src/app/recu/recu.component.ts
+++ src/app/recu/recu.component.ts
@@ -15,13 +15,23 @@ code=this.route.snapshot.params['code']
 montant=this.route.snapshot.params['mont']
 libelle=this.route.snapshot.params['libelle']
 res:any
+
   ngOnInit(): void {
 
     this.info.recupinfoetudiant(this.code).subscribe(data=>{
       this.res=data
     })
     console.log(this.user)
+    console.log(this.numCommande)
   }
+  public a = Math.floor(Math.random()*10);
+  public r = Math.floor(Math.random()*10);
+  public b = Math.floor(Math.random()*10);
+  public c = Math.floor(Math.random()*10);
+  public d = Math.floor(Math.random()*10);
+  public e = Math.floor(Math.random()*10);
+  numCommande=this.a +'' +this.b +''+ this.r +'' +this.c+this.d+this.e
+
   ele:string="impression"
    PrintElem(elem:any){
     var mywindow :any = window.open('', 'PRINT', 'height=900,width=1300');

51c359ca2c578a7c5a275a7f13f6b83c6a67a8eb
Ibrahima Miniane Diouf <ibrahima.diouf@myriadgroup.com>
2023-04-13 11:20:46 +0000

    reglage

--- src/app/services/info-utilisateur.service.ts
+++ src/app/services/info-utilisateur.service.ts
@@ -9,15 +9,9 @@ import { Observable, observable
 export class InfoUtilisateurService {
   public envoie:any
   public data:any=new FormData()
-<<<<<<< HEAD
   public apiurl="https://coral-app-a57e6.ondigitalocean.app/personnel"
   public apiurl1="https://coral-app-a57e6.ondigitalocean.app/personnel/update"
   public apiurl2="https://coral-app-a57e6.ondigitalocean.app/personnel/updateprofile"
-=======
-  public apiurl="http://104.236.2.144/personnel"
-  public apiurl1="http://104.236.2.144/personnel/update"
-  public apiurl2="http://104.236.2.144/personnel/updateprofile"
->>>>>>> parent of cfd754c ( last essai)
   constructor(private http:HttpClient) {
    }
    recupinfo(id:string):Observable<any>{
--- src/app/services/paiement.service.ts
+++ src/app/services/paiement.service.ts
@@ -7,21 +7,12 @@ import { Observable,
   providedIn: 'root'
 })
 export class PaiementService {
-<<<<<<< HEAD
   public apiurl='https://coral-app-a57e6.ondigitalocean.app/facture'
   public apiurl2='https://coral-app-a57e6.ondigitalocean.app/facture/debiteur'
   public apiurl1='https://coral-app-a57e6.ondigitalocean.app/etudiant/mign'
   public apiurl3='https://coral-app-a57e6.ondigitalocean.app/facture/getfacture'
   public apiurl4='https://coral-app-a57e6.ondigitalocean.app/facture/maxfacture'
   public apiurl5='https://coral-app-a57e6.ondigitalocean.app/facture/onefacture'
-=======
-  public apiurl='http://104.236.2.144/facture'
-  public apiurl2='http://104.236.2.144/facture/debiteur'
-  public apiurl1='http://104.236.2.144/etudiant/mign'
-  public apiurl3='http://104.236.2.144/facture/getfacture'
-  public apiurl4='http://104.236.2.144/facture/maxfacture'
-  public apiurl5='http://104.236.2.144/facture/onefacture'
->>>>>>> parent of cfd754c ( last essai)
 
   public reponse:any
   public envoie:any
